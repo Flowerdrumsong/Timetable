5357967c5a016ed94e47622c17d24bca402fa9ae
Flowerdrumsong <hello7hm@gmail.com>
2022-07-14 18:05:19 +0900

    adding text

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
   const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
-  ctx.beginPath();
-  ctx.moveTo(150,150);
-  ctx.arc(150, 150, 150, 0, 360, false);
-  ctx.fillStyle = "rgba(0, 0, 0, .2)";
-  ctx.fill();
-  
   const add_btn=document.getElementById('add_btn');
-  
+  const ctx = canvas.getContext('2d');
+
+  if(canvas.getContext){
+    ctx.beginPath();
+    ctx.moveTo(150,150);
+    ctx.arc(150, 150, 149, 0, 360, false);
+    ctx.strokeStyle = "rgba(0, 0, 0, .5)";
+    ctx.stroke();
+  }else{
+    alert("canvas-unsupported");
+  }
+
   add_btn.addEventListener('click',function(){
     const start_time=document.getElementById('start_time').value;
     const end_time=document.getElementById('end_time').value;
@@ -16,80 +20,77 @@ document.addEventListener("DOMContentLoaded", function(){
     convert(start_time, end_time, todo);
   })
 
-  function convert(start_time, end_time){
+  delete_btn.addEventListener('click',function(){
+    
+  })
+
+  //시간계산
+  function convert(start_time, end_time, todo){
     let start_time_h=start_time.substr(0,2);
     let start_time_m=start_time.substr(3,2);
     let end_time_h=end_time.substr(0,2);
     let end_time_m=end_time.substr(3,2);
     console.log(start_time_h);
     console.log(end_time_h);
-    if(end_time_h<start_time_h){
-      alert('Reset the time')
-    }else{
+    // if(end_time_h<start_time_h){
+    //   alert('시간이 이상해요!');
+    // }else{
+      write(todo);
+      divide_time(start_time_h, start_time_m, end_time_h, end_time_m,todo);
       if(start_time_h>5){
-        var angle1=(start_time_h-6)*15;
-        var angle2=start_time_m*0.25;
-        var start_degrees=angle1+angle2;
-
-        var angle3=(end_time_h-6)*15;
-        var angle4=end_time_m*0.25;
-        var end_degrees=angle3+angle4;
-
+        const start_degrees=(start_time_h-6)*15+start_time_m*0.25;
+        const end_degrees=(end_time_h-6)*15+end_time_m*0.25;
+        // divide_time(start_time_h, start_time_m, end_time_h, end_time_m,todo);
         draw(start_degrees, end_degrees);
-        write(start_degrees, end_degrees,todo.value);
       }else if(start_time_h<=5){
         start_time_h=parseInt(start_time.substr(1,1));
         start_time_m=start_time.substr(3,2);
         end_time_h=parseInt(end_time.substr(1,1));
         end_time_m=end_time.substr(3,2);
-
-        var angle1=(start_time_h+19)*15;
-        var angle2=start_time_m*0.25;
-        var start_degrees=angle1+angle2;
-
-        var angle3=(end_time_h+19)*15;
-        var angle4=end_time_m*0.25;
-        var end_degrees=angle3+angle4;
-
+        const start_degrees=(start_time_h+19)*15+start_time_m*0.25;
+        const end_degrees=(end_time_h+19)*15+end_time_m*0.25;
+        // divide_time(start_time_h, start_time_m, end_time_h, end_time_m,todo);
         draw(start_degrees, end_degrees);
-        write(start_degrees, end_degrees,todo.value);
-      }
+      // }
     }
   }
 
-  
+  function divide_time(start_time_h, start_time_m, end_time_h, end_time_m,todo){
+    const middle_h=(parseInt(start_time_h)+parseInt(end_time_h))/2;
+    const middle_m=(parseInt(start_time_m)+parseInt(end_time_m))/2;
+    draw_todo(middle_h, middle_m, todo);
+  }
+
+  //캔버스에 색상 채우기
   function draw(start_degrees, end_degrees){
-    if(canvas.getContext){
-      //각도를 degree가 아닌 radian으로 사용하기 때문에 계산 필요
-      //라디안: 반지름에 대한 호의 길이의 비
-      const start_angle=(Math.PI/180)*start_degrees;
-      const end_angle=(Math.PI/180)*end_degrees;
-      ctx.beginPath();
-      ctx.moveTo(150,150);
-      ctx.arc(150, 150, 150, start_angle, end_angle, false);
-      ctx.fillStyle = "rgba(255, 165, 0, .5)";
-      ctx.fill();
-    }else{
-      //canvas-unsupported code here
-    
-    }
+    //각도를 degree가 아닌 radian으로 사용하기 때문에 계산 필요
+    //라디안: 반지름에 대한 호의 길이의 비
+    const start_angle=(Math.PI/180)*start_degrees;
+    const end_angle=(Math.PI/180)*end_degrees;
+    ctx.beginPath();
+    ctx.moveTo(150,150);
+    ctx.arc(150, 150, 150, start_angle, end_angle, false);
+    ctx.fillStyle = 'rgba(' + Math.floor(255 - 42.5 * Math.floor(Math.random()*7)) + ', ' + Math.floor(255 - 42.5 * Math.floor(Math.random()*7)) + ', '+Math.floor(255 - 42.5 * Math.floor(Math.random()*7))+', .5)';
+    ctx.fill();
   }
-  function write(start_degrees, end_degrees, todo){
-    if(canvas.getContext){
-      const ctx = canvas.getContext('2d');
-      const start_angle=(Math.PI/180)*start_degrees;
-      const end_angle=(Math.PI/180)*end_degrees;
-      const todo_angle=(start_angle+end_angle)/2;
-      ctx.font = '20px serif';
-      //x,y좌표값 계산해서 넣기
-      ctx.fillText(todo, 10, 10);
-      //x,y좌표값 계산해서 넣기
-      listing(todo);
-    }
+  //캔버스에 글씨 쓰기
+  //시간을 받아서
+  function draw_todo(middle_h, middle_m, todo){
+    //시간을 나눠서 좌표 구하기
+    const middle_degrees=(middle_h-6)*15+middle_m*0.25;
+    const x=parseInt(130*Math.cos(Math.PI/180*middle_degrees));
+    const y=parseInt(130*Math.sin(Math.PI/180*middle_degrees));
+    const ctx = canvas.getContext('2d');
+    console.log(x,y);
+    ctx.style="black";
+    ctx.fillText("!!!!!!", x+150, y+150);
   }
-  function listing(todo){
-    const list=document.getElementById('list');
-    list.innerHTML+='<li><span>'+todo+'</span><button class="delete_btn">X</button></li>';
+
+  //리스트에 쓰기
+  function write(todo){
+    if(canvas.getContext){
+      list.innerHTML+='<li class="clearfix"><span>'+todo+'</span><button class="delete_btn">X</button></li>';
+    }
   }
 
 });
